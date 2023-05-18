f0be03a418d2653172bb378ab7bb2cc7c099594d
Parle648 <sashahoumi@gmail.com>
2023-05-18 15:11:05 +0300

    init: create function for create sliders

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,27 +3,7 @@ const slederItems = document.querySelectorAll('.slider__item');
 const sliderLine = document.querySelector('.photo-slider__line');
 const sliderBtns = document.querySelectorAll('.slider__btn');
 
+// createSlider function
+import {createSlider} from './ui_functions.js'
 
-sliderLine.style.width = `${269 * slederItems.length}px`
-
-slederItems.forEach((item) => {
-    if ( item.getAttribute('data-head-item') != 'first' ) {
-        item.classList.add('secondary-items')
-    }
-})
-
-let count = 0;
-let margin = 0;
-
-sliderBtns[0].addEventListener('click', () => {
-    if ( count < 5 ) {
-        margin += sliderLine.offsetWidth / slederItems.length;
-        sliderLine.style.margin = `0px 0px 0px ${-margin}px`;
-        console.log('it\'s working');
-        count++;
-    } else {
-        margin = 0;
-        sliderLine.style.margin = `0px 0px 0px ${-margin}px`;
-        count = 0;
-    }
-})
+createSlider(sliderLine, slederItems)
---
+++ src/scripts/ui_functions.js
@@ -0,0 +1,49 @@
+export function createSlider(line, items, maxCounter = items.length - 2) {
+    line.style.width = `${269 * items.length}px`
+
+    items.forEach((item) => {
+        if ( item.getAttribute('data-head-item') != 'first' ) {
+            item.classList.add('secondary-items')
+        }
+    })
+
+    let count = 0;
+    let margin = 0;
+
+    sliderBtns[0].addEventListener('click', () => {
+        if ( count <= maxCounter ) {
+            items[count + 1].classList.remove('secondary-items');
+            margin -= 269 + 67;
+            line.style.margin = `0px 0px 20px ${margin}px`;
+            count++;
+        } else {
+            margin = 0;
+            line.style.margin = `0px 0px 20px ${margin}px`;
+            count = 0;
+            // add style for make images little
+            items.forEach((item) => {
+                if ( item.getAttribute('data-head-item') != 'first' ) {
+                    item.classList.add('secondary-items')
+                }
+            })
+        }
+    })
+
+    sliderBtns[1].addEventListener('click', () => {
+        if ( count > 0 ) {
+            items[count].classList.add('secondary-items');
+            margin += (269 + 67);
+            line.style.margin = `0px 0px 20px ${margin}px`;
+            count--;
+        } else {
+            margin = -1008;
+            line.style.margin = `0px 0px 20px ${margin}px`;
+            count = maxCounter;
+            // add style for make images little
+            items.forEach((item) => {
+                item.classList.remove('secondary-items')
+            })
+            items[items.length - 1].classList.add('secondary-items')
+        }
+    })
+}
