d1f3bc7cce44c3a895ea2ee6d167bee43bd38bd5
Parle648 <sashahoumi@gmail.com>
2023-05-31 18:31:08 +0300

    init: make day blocks

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,46 @@
-const slider = document.querySelector('.photo-slider');
 const slederItems = document.querySelectorAll('.slider__item');
 const sliderLine = document.querySelector('.photo-slider__line');
 const sliderBtns = document.querySelectorAll('.slider__btn');
 
-const firstDaySlider = document.querySelector('.day-one__photo-slider');
 const firstDaySlederItems = document.querySelectorAll('.day-one__slider__item');
 const firstDaySliderLine = document.querySelector('.day-one__photo-slider__line');
 const firstDaySliderBtns = document.querySelectorAll('.one-day__slider-btn');
+
+const secondDaySlederItems = document.querySelectorAll('.day-two__slider__item');
+const secondDaySliderLine = document.querySelector('.day-two__photo-slider__line');
+const secondDaySliderBtns = document.querySelectorAll('.two-day__slider-btn');
+
+const thirdDaySlederItems = document.querySelectorAll('.day-three__slider__item');
+const thirdDaySliderLine = document.querySelector('.day-three__photo-slider__line');
+const thirdDaySliderBtns = document.querySelectorAll('.three-day__slider-btn');
+
+const fourthDaySlederItems = document.querySelectorAll('.day-four__slider__item');
+const fourthDaySliderLine = document.querySelector('.day-four__photo-slider__line');
+const fourthDaySliderBtns = document.querySelectorAll('.four-day__slider-btn');
+
+const fifthDaySlederItems = document.querySelectorAll('.day-fifth__slider__item');
+const fifthDaySliderLine = document.querySelector('.day-fifth__photo-slider__line');
+const fifthDaySliderBtns = document.querySelectorAll('.fifth-day__slider-btn');
+
+const sixthDaySlederItems = document.querySelectorAll('.day-sixth__slider__item');
+const sixthDaySliderLine = document.querySelector('.day-sixth__photo-slider__line');
+const sixthDaySliderBtns = document.querySelectorAll('.sixth-day__slider-btn');
+
+const sevenDaySlederItems = document.querySelectorAll('.day-seven__slider__item');
+const sevenDaySliderLine = document.querySelector('.day-seven__photo-slider__line');
+const sevenDaySliderBtns = document.querySelectorAll('.seven-day__slider-btn');
 
 // createSlider function
 import {createSlider} from './ui_functions.js'
 
 createSlider(sliderLine, slederItems, sliderBtns);
 createSlider(firstDaySliderLine, firstDaySlederItems, firstDaySliderBtns);
+createSlider(secondDaySliderLine, secondDaySlederItems, secondDaySliderBtns);
+createSlider(thirdDaySliderLine, thirdDaySlederItems, thirdDaySliderBtns);
+createSlider(fourthDaySliderLine, fourthDaySlederItems, fourthDaySliderBtns);
+createSlider(fifthDaySliderLine, fifthDaySlederItems, fifthDaySliderBtns);
+createSlider(sixthDaySliderLine, sixthDaySlederItems, sixthDaySliderBtns);
+createSlider(sevenDaySliderLine, sevenDaySlederItems, sevenDaySliderBtns);
 
 fetch(`http://gutendex.com/books?topic=Adventure&languages=en`)
 .then(request => request.text())
--- src/scripts/ui_functions.js
+++ src/scripts/ui_functions.js
@@ -1,18 +1,18 @@
 export function createSlider(line, items, btns, maxCounter = items.length - 2) {
     line.style.width = `${269 * items.length}px`
 
-    items.forEach((item) => {
-        if ( item.getAttribute('data-head-item') != 'first' ) {
-            item.classList.add('secondary-items')
-        }
-    })
+    // items.forEach((item) => {
+    //     if ( item.getAttribute('data-head-item') != 'first' ) {
+    //         item.classList.add('secondary-items')
+    //     }
+    // })
 
     let count = 0;
     let margin = 0;
 
     btns[0].addEventListener('click', () => {
-        if ( count <= maxCounter ) {
-            items[count + 1].classList.remove('secondary-items');
+        if ( count < maxCounter ) {
+            // items[count + 1].classList.remove('secondary-items');
             margin -= 269 + 67;
             line.style.margin = `0px 0px 20px ${margin}px`;
             count++;
@@ -21,17 +21,17 @@ export function createSlider(line, items, btns, maxCounter = items.length - 2) {
             line.style.margin = `0px 0px 20px ${margin}px`;
             count = 0;
             // add style for make images little
-            items.forEach((item) => {
-                if ( item.getAttribute('data-head-item') != 'first' ) {
-                    item.classList.add('secondary-items')
-                }
-            })
+            // items.forEach((item) => {
+            //     if ( item.getAttribute('data-head-item') != 'first' ) {
+            //         item.classList.add('secondary-items')
+            //     }
+            // })
         }
     })
 
     btns[1].addEventListener('click', () => {
         if ( count > 0 ) {
-            items[count].classList.add('secondary-items');
+            // items[count].classList.add('secondary-items');
             margin += (269 + 67);
             line.style.margin = `0px 0px 20px ${margin}px`;
             count--;
@@ -40,10 +40,10 @@ export function createSlider(line, items, btns, maxCounter = items.length - 2) {
             line.style.margin = `0px 0px 20px ${margin}px`;
             count = maxCounter;
             // add style for make images little
-            items.forEach((item) => {
-                item.classList.remove('secondary-items')
-            })
-            items[items.length - 1].classList.add('secondary-items')
+            // items.forEach((item) => {
+            //     item.classList.remove('secondary-items')
+            // })
+            // items[items.length - 1].classList.add('secondary-items')
         }
     })
 }
